b6b8ddbdb1807b6181a34ab5958396ee831d080f
ocb-dev-04 <42700726+ocb-dev-04@users.noreply.github.com>
2019-05-08 23:59:58 -0400

    *Add print and variables files

---
+++ go/src/github.com/ocb_04/go_crash_course/02_Variables/variables.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var x, y, z int
+	// se le asgina un tipo cadena
+	name := "Fulano"
+	fmt.Println("El valor inicial de la variable es este: " + name)
+	name = "Fulanito"
+	fmt.Println("El valor final de la variable es este: " + name)
+}
